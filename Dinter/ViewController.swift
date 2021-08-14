4c3a3422566e0a761f1286d4e4a2178c39bd81cb
Luis Segoviano <the.segoviano@gmail.com>
2021-08-14 11:07:27 -0500

    Creando proyecto

---
+++ Dinter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dinter
+//
+//  Created by Luis Segoviano on 14/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
